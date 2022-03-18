fe615b4d468ea7cc55b2f835236a98664ef1f86a
brandynbennett <brandynlbennett@gmail.com>
2022-03-17 22:42:35 -0600

    create add_fact function for genserver

--- lib/fact_engine/boundary/fact_manager.ex
+++ lib/fact_engine/boundary/fact_manager.ex
@@ -1,12 +1,61 @@
 defmodule FactEngine.Boundary.FactManager do
-  use GenServer
+  @moduledoc """
 
+  """
+  use GenServer
+  alias FactEngine.Core.Fact
+
+  @doc """
+  Create a new FactManager
+  """
   def new(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  @doc """
+  Add a fact to the Manager
+
+  ## Examples
+      iex> FactManager.add_fact("are_friends (alex, sam)")
+      :ok
+
+      iex> FactManager.add_fact("foo")
+      {:error, "foo is not a valid fact"}
+
+  """
+  def add_fact(fact) do
+    GenServer.call(__MODULE__, {:add_fact, fact})
+  end
+
+  @doc """
+  Get all the facts the manager knows about
+
+  ## Examples
+      iex> FactManager.get_facts()
+      [%Fact{}, %Fact{}]
+  """
+  def get_facts do
+    GenServer.call(__MODULE__, :get_facts)
   end
 
   @impl true
   def init(state \\ []) do
     {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:add_fact, fact}, _from, state) do
+    case Fact.from_string(fact) do
+      {:ok, fact} ->
+        {:reply, :ok, Enum.concat(state, [fact])}
+
+      error ->
+        {:reply, error, state}
+    end
+  end
+
+  @impl true
+  def handle_call(:get_facts, _from, state) do
+    {:reply, state, state}
   end
 end
--- test/boundary/fact_manager_test.exs
+++ test/boundary/fact_manager_test.exs
@@ -2,9 +2,23 @@ defmodule FactEngine.Boundary.FactManagerTest do
   use ExUnit.Case, async: false
 
   alias FactEngine.Boundary.FactManager
+  alias FactEngine.Core.Fact
 
   test "new/0 creates a new Manager" do
     FactManager.new()
     assert GenServer.whereis(FactManager) |> is_pid()
+  end
+
+  test "add_fact/1 adds a fact" do
+    FactManager.new()
+    assert FactManager.add_fact("are_friends (alex, sam)") == :ok
+
+    assert [%Fact{statement: "are_friends", arguments: ["alex", "sam"]}] ==
+             FactManager.get_facts()
+  end
+
+  test "add_fact/1 gives error for invalid fact" do
+    FactManager.new()
+    assert FactManager.add_fact("foo") == {:error, "foo is not a valid fact"}
   end
 end

7fa13375de965cfe369861a84977e7c163bc6b67
brandynbennett <brandynlbennett@gmail.com>
2022-03-19 09:47:44 -0600

    config mock

---
+++ config/test.exs
@@ -0,0 +1,3 @@
+import Config
+
+config :fact_engine_cli, :file, FactEngineCLI.FileMock
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
+Mox.defmock(FactEngineCLI.FileMock, for: FactEngineCLI.FileBehaviour)
+Mox.stub_with(FactEngineCLI.FileMock, FactEngineCLI.FileStub)
+
 ExUnit.start()
